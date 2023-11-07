ebcf26a87dce241248dbf4c82ec266b7d7801b1a
Munaf Hajir <munafhajirpro@gmail.com>
2023-11-07 14:20:28 +0530

    Added API Integration and Component Lifecycle

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,26 +3,48 @@ import { Component } from "react";
 class Product extends Component {
   constructor(props) {
     super(props);
+
+    this.data = {
+
+    }
+    this.state = {
+      productList: [],
+    };
+  }
+
+  getProductList = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => this.setState({ productList: json }));
+  };
+
+  componentDidMount(){
+    this.getProductList()
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("Update", {prevProps, prevState})
   }
 
   render() {
     return (
       <div>
         <h1> Product </h1>
-        <div>
-          <img />
-          <h2>Product : Name</h2>
-          <h3>Price: 100</h3>
-          <h5> Category: </h5>
-          <p>Description: </p>
-        </div>
-        <div>
-          <img />
-          <h2>Product : Name</h2>
-          <h3>Price: 100</h3>
-          <h5> Category: </h5>
-          <p>Description: </p>
-        </div>
+        {
+          this.state.productList.map(
+            (value,index) => {
+              return (
+                <div key={value.title + index}>
+                  <img src={value.image} style={{height: "250px"}}/>
+                  <h2>Product : {value.title}</h2>
+                  <h3>Price: {value.price}</h3>
+                  <h5> Category: </h5>
+                  <p>Description: </p>
+                </div>
+              )
+            }
+          )
+        }
       </div>
     );
   }
--- state.js
+++ state.js
@@ -2,6 +2,6 @@
 import {useState} from 'react'
 
 const [currentValue,changeValue] = useState(initialValue)
-
+currentValue = 123
 
 changeValue('value')
